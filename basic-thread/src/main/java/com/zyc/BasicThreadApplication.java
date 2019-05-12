66428dbcf93a980416916cec9620097b914bdd44
zhangyinchao <292306404@qq.com>
2019-05-12 22:50:35 +0800

    commit thread desing01

---
+++ basic-thread/src/main/java/com/zyc/BasicThreadApplication.java
@@ -0,0 +1,13 @@
+package com.zyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicThreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicThreadApplication.class, args);
+	}
+
+}
