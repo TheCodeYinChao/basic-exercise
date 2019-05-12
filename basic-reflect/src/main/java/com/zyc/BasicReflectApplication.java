67012d12fd2a73c2a5797ec3771fbf528be35089
zhangyinchao <292306404@qq.com>
2019-05-12 11:21:26 +0800

    first commit

---
+++ basic-reflect/src/main/java/com/zyc/BasicReflectApplication.java
@@ -0,0 +1,13 @@
+package com.zyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicReflectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicReflectApplication.class, args);
+	}
+
+}
