51cf304677ca186305399f2e7ea82440d46f72f1
TheCodeYinChao <292306404@qq.com>
2019-12-11 15:00:20 +0800

    死循环到导致的cpu飚高

---
+++ basic-thread/src/main/java/com/zyc/juclock/LoopDead.java
@@ -0,0 +1,39 @@
+package com.zyc.juclock;
+
+/**
+ * 死循环会占用cpu 导致cpu飚高 这个跟cpu核数有关
+ *
+ * 可以通过相关方法 sleep yield wait await park 让出cpu 的执行权
+ */
+public class LoopDead {
+    public static void main(String[] args) throws Exception {
+
+        while (true){
+//            Thread.sleep(1000);
+//            Thread.yield();
+//            Object.wait();
+//            await
+//            park
+        }
+
+
+
+//        new  Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                while (true){}
+//            }
+//        }).start();
+//
+//        new  Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                while (true){}
+//            }
+//        }).start();
+//
+
+
+
+    }
+}
