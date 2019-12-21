3d91b521477a6830ba4131cf02382d3fcf0773af
zhangyinchao <292306404@qq.com>
2019-12-21 23:20:22 +0800

    添加源码模块

---
+++ basic-soundcode/src/main/java/com/zyc/SoundcodeApplication.java
@@ -0,0 +1,13 @@
+package com.zyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundcodeApplication.class, args);
+	}
+
+}
