ef68acc51ce2c7fedf653330c3cf25fa1cef4afa
TheCodeYinChao <292306404@qq.com>
2019-12-24 18:49:07 +0800

    mybaiteis 责任链模式

--- basic-soundcode/src/main/java/com/zyc/mybaties/intercept/InterceptorChain.java
+++ basic-soundcode/src/main/java/com/zyc/mybaties/intercept/InterceptorChain.java
@@ -4,13 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InterceptorChain {
-    private List<Intercept> intercepts =new ArrayList<>();
+    private List<Intercept> intercepts;
 
+    public InterceptorChain(List<Intercept> intercepts) {
+        this.intercepts = intercepts;
+    }
 
     public Object registerAll(Object target){
         for(Intercept intercept:intercepts){
             target = intercept.register(target);
         }
         return target;
+    }
+
+    public static void main(String[] args) {
+        List<Intercept> interceptors = new ArrayList<Intercept>();
+        Intercept logInterceptor = new IntercepterImpl();
+        interceptors.add(logInterceptor);
+        InterceptorChain interceptorChain = new InterceptorChain(interceptors);
+
+        Target target = new TargetImpl();
+        target= (Target)interceptorChain.registerAll(target);
+        target.execute();
     }
 }
