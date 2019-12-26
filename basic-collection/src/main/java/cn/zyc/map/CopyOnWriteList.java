ae3455f3e9ac9865b5ec572fd22bd6cc10b46c65
TheCodeYinChao <292306404@qq.com>
2019-12-26 11:37:51 +0800

    copy  on  write

---
+++ basic-collection/src/main/java/cn/zyc/map/CopyOnWriteList.java
@@ -0,0 +1,19 @@
+package cn.zyc.map;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class CopyOnWriteList {
+
+    public static void main(String[] args) {
+
+        CopyOnWriteArrayList<String> c = new CopyOnWriteArrayList<String>();
+        for (int i = 0;i<10 ; i++) {
+            c.add("i"+i);
+        }
+
+
+        c.remove(0);
+
+        c.get(0);
+    }
+}
