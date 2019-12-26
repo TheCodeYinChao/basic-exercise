c168430427cc697f47a97e24c7def7d6593e73df
TheCodeYinChao <292306404@qq.com>
2019-12-26 11:27:22 +0800

    取模算法

--- basic-thread/src/main/java/com/zyc/bitopera/Bit.java
+++ basic-thread/src/main/java/com/zyc/bitopera/Bit.java
@@ -11,6 +11,9 @@ public class Bit {
      * 同时为了保证节点的分布均匀，我们会虚拟出很多个节点
      *
      * 对于容器相关用到很多hash算法 并且保证了hash算法的均匀散布
+     *
+     * 取模算法
+
      * @param args
      */
     public static void main(String[] args) {
@@ -19,12 +22,23 @@ public class Bit {
         System.out.println(hash);
 
         int i = 16 & hash;
-
         System.out.println(i);
+
+        int getmo = getmo(14, hash);
+
+        System.out.println(getmo);
 
 
     }
     static final int spread(int h) {
         return (h ^ (h >>> 16)) & 0x7fffffff;
+    }
+
+
+
+    static final  int getmo(int a,int h){
+        int spread = spread(h);
+        int i = spread%a;
+        return i;
     }
 }
