6f86366621c8da0658f33ac6d25a749cf00e256b
TheCodeYinChao <292306404@qq.com>
2019-12-26 13:44:39 +0800

    cch

---
+++ basic-thread/src/main/java/com/zyc/juccollcetion/ConCurrentHashMap.java
@@ -0,0 +1,15 @@
+package com.zyc.juccollcetion;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConCurrentHashMap {
+
+    public static void main(String[] args) {
+
+        ConcurrentHashMap c = new ConcurrentHashMap();
+
+
+        c.put("a","A");
+
+    }
+}
