cf1cf6d7e7e7e4b85b1a19f6ea5212f4d286c02f
zhangyinchao <292306404@qq.com>
2019-12-29 23:11:06 +0800

    tijiao

---
+++ basic-soundcode/src/main/java/com/zyc/mybaties/init/MapperDemo.java
@@ -0,0 +1,10 @@
+package com.zyc.mybaties.init;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * Created by Admin on 2019/12/29.
+ */
+public class MapperDemo extends SqlSessionDaoSupport{
+}
