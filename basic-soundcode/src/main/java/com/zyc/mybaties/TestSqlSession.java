c50a716a6ebbc891e3b9fc5a45aa2d656202483f
TheCodeYinChao <292306404@qq.com>
2020-01-02 16:06:17 +0800

    修改关于缓存队列的实现

--- basic-soundcode/src/main/java/com/zyc/mybaties/TestSqlSession.java
+++ basic-soundcode/src/main/java/com/zyc/mybaties/TestSqlSession.java
@@ -4,16 +4,19 @@ package com.zyc.mybaties;
 import org.apache.ibatis.cache.decorators.BlockingCache;
 import org.apache.ibatis.cache.decorators.LruCache;
 import org.apache.ibatis.cache.decorators.ScheduledCache;
+import org.apache.ibatis.cache.decorators.WeakCache;
 import org.apache.ibatis.cache.impl.PerpetualCache;
 import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.logging.LogFactory;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
-import org.mybatis.spring.annotation.MapperScan;
 
+import java.awt.*;
 import java.io.InputStream;
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
 import java.util.List;
 import java.util.Map;
 
@@ -52,5 +55,36 @@ public class TestSqlSession {
         lruCache.setSize(1);
         lruCache.putObject("aa","aa");
         lruCache.putObject("a2","aa");
+    }
+
+
+    @Test
+    public void gcTest(){
+
+        WeakCache weakCache = new WeakCache(new PerpetualCache("11"));
+        weakCache.putObject("aa","aad");
+
+
+        ReferenceQueue<Object> objectReferenceQueue = new ReferenceQueue<Object>(); //回收引用队列
+
+        Object o1 = new Object();
+        WeakReference<Object> owr = new WeakReference<Object>(o1, objectReferenceQueue);//弱引用对象 当有gc发生时回收
+        Object o2 = owr.get();
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                WeakReference<Object> poll;
+                while ((poll = (WeakReference<Object>)objectReferenceQueue.poll()) !=null){
+                    Object o1 = poll.get();
+                    System.out.println("o1:"+o1);
+                }
+            }
+        });
+
+        thread.setDaemon(true);
+        thread.start();
+        System.gc();
+        Object o = owr.get();
     }
 }
