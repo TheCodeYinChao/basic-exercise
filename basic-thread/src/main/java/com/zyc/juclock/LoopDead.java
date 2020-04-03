074b0b06ae147d41001bebff7563f20f4f604a97
TheCodeYinChao <292306404@qq.com>
2020-04-03 19:35:14 +0800

    添加 spring 动态代理

--- basic-thread/src/main/java/com/zyc/juclock/LoopDead.java
+++ basic-thread/src/main/java/com/zyc/juclock/LoopDead.java
@@ -3,7 +3,7 @@ package com.zyc.juclock;
 /**
  * 死循环会占用cpu 导致cpu飚高 这个跟cpu核数有关
  *
- * 可以通过相关方法 sleep yield wait await park 让出cpu 的执行权
+ * 可以通过相关方法 sleep（有待商榷） yield wait await park 让出cpu 的执行权
  */
 public class LoopDead {
     public static void main(String[] args) throws Exception {
