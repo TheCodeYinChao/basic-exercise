993ffffee1b2f3342c354eaf7c180ac145892395
TheCodeYinChao <292306404@qq.com>
2020-04-21 13:05:25 +0800

    面试题

---
+++ basic-jvm/src/main/java/cn/zyc/jvm/Test.java
@@ -0,0 +1,17 @@
+package cn.zyc.jvm;
+
+/**
+ * description: Test <br>
+ * date: 2020/4/21 13:02 <br>
+ * author: zyc <br>
+ * version: 1.0 <br>
+ *     int  0  编程 integer 是null  去掉静态报错啦
+ *
+ */
+public class Test {
+    static int i;
+
+    public static void main(String[] args) {
+        System.out.println(i);
+    }
+}
