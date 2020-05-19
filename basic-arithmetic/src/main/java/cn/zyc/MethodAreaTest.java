cde2740205d69aff54e89e357bbd493a80787e3d
TheCodeYinChao <292306404@qq.com>
2020-05-19 10:40:06 +0800

    面试题

---
+++ basic-arithmetic/src/main/java/cn/zyc/MethodAreaTest.java
@@ -0,0 +1,34 @@
+package cn.zyc;
+
+/**
+ * description: MethodAreaTest <br>
+ * date: 2020/5/18 17:55 <br>
+ * author: zyc <br>
+ * version: 1.0 <br>
+ */
+public class MethodAreaTest {
+    public static void main(String[] args) {
+        Order order = null;
+        System.out.println(order.count);
+        System.out.println(order.count);
+
+    }
+}
+
+class Order{
+    public static int count  = 1;
+
+
+    public int aa;
+
+
+    public static App a  = new App();
+
+    static {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ basic-arithmetic/src/main/java/cn/zyc/SyncDemo.java
@@ -0,0 +1,30 @@
+package cn.zyc;
+
+/**
+ * description: SyncDemo <br>
+ * date: 2020/5/18 19:05 <br>
+ * author: zyc <br>
+ * version: 1.0 <br>
+ */
+public class SyncDemo {
+    private SyncDemo(){}
+
+    private static SyncDemo syncDemo;
+
+    public static SyncDemo getinstance(){
+        if(syncDemo == null){
+            synchronized (SyncDemo.class){
+                if(syncDemo == null){
+                    syncDemo = new SyncDemo();
+                }
+            }
+        }
+        return syncDemo;
+    }
+
+
+    public static void main(String[] args) {
+
+        SyncDemo.getinstance();
+    }
+}
