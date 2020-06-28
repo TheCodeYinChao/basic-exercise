c66af539ac0acf756cba33948e3ce85ed1291d1b
TheCodeYinChao <292306404@qq.com>
2020-06-28 20:43:30 +0800

    nginx config

--- basic-soundcode/src/main/java/com/zyc/mybaties/dynamicproxy/ProxyDynamic.java
+++ basic-soundcode/src/main/java/com/zyc/mybaties/dynamicproxy/ProxyDynamic.java
@@ -1,14 +1,20 @@
 package com.zyc.mybaties.dynamicproxy;
 
+
 import java.lang.reflect.Proxy;
 
 public class ProxyDynamic {
     public static void main(String[] args) {
+
         Ihello ihello = new IhelloImpl();
 
         InvocationHandler handler = new InvocationHandler(ihello);
         System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
-        Ihello o = (Ihello)Proxy.newProxyInstance(IhelloImpl.class.getClassLoader(), IhelloImpl.class.getInterfaces(), handler);
+        Class<?>[] interfaces = Ihello.class.getInterfaces();
+
+        Class[] classes = {Ihello.class};
+
+        Ihello o = (Ihello)Proxy.newProxyInstance(Ihello.class.getClassLoader(),  classes, handler);
         o.sayHello();
 
     }
