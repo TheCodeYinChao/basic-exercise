93267c1a6486dc8f34d30bb1af5d3216c9457349
TheCodeYinChao <292306404@qq.com>
2020-11-03 14:49:59 +0800

    自定义java 解析类

---
+++ basic-arithmetic/src/main/java/cn/zyc/jcostumcomlper/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * description: package-info
+ *
+ * 对于 web面板中传递java代码执行获取结果的技术的探讨
+ *
+ * 由jdk1.6之后提供的自编译来实现
+ *
+    Test3.java
+ * 中相比于 CustomStringJavaCompiler
+ * 增加了安全性机制，防止服务器崩溃 比如 死循环之类的代码
+ * date: 2020/11/3 14:46 <br>
+ * author: zyc <br>
+ * version: 1.0 <br>
+ */
+package cn.zyc.jcostumcomlper;
