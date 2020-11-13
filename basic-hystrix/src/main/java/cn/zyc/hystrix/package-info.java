fcff0921f032ad169abf7584e21b17c83c95f21e
TheCodeYinChao <292306404@qq.com>
2020-11-13 18:30:38 +0800

    hystrix 熔断

--- basic-hystrix/src/main/java/cn/zyc/hystrix/package-info.java
+++ basic-hystrix/src/main/java/cn/zyc/hystrix/package-info.java
@@ -2,6 +2,8 @@
  * dsc: package-info
  * @date: 2020/11/13 18:07
  * @author: zyc
+ * @see cn.zyc.hystrix.HelloWorldCommand#getFallback()
+ * <tt>URLClassLoader#findClass</tt>实现为
  *<a href="https://github.com/star2478/java-hystrix"/>
  */
 package cn.zyc.hystrix;
