a375ecbdb22a0f3e5b885f9d5b7bf7545ad2a614
TheCodeYinChao <292306404@qq.com>
2020-11-26 16:38:30 +0800

    添加queue的uml图

---
+++ basic-thread/src/main/java/com/zyc/jucqueue/ExampleQueue.java
@@ -0,0 +1,39 @@
+package com.zyc.jucqueue;
+
+import org.junit.Test;
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * dsc: ExampleQueue
+ * date: 2020/11/26 16:05
+ * author: zyc
+ */
+public class ExampleQueue {
+    @Test
+    public void baseUser(){
+        Queue queue = new ArrayBlockingQueue(2);
+        queue.add(1);
+//        queue.add(2);//满 抛出异常
+        queue.offer(2);//满 返回false
+
+    /*    for(int i = 0;i<4;i++){
+            Object peek1 = queue.element();
+            System.out.println(peek1);
+            Object peek = queue.peek();
+            System.out.println(peek);
+        }*/
+
+        for(int i = 0;i<4;i++){
+//            Object peek1 = queue.remove();
+//            System.out.println(peek1);
+            Object peek = queue.poll();
+            System.out.println(peek);
+        }
+
+    }
+
+
+
+}
