81a6c117a7198df3662b52a8dda212e8aee8f3c0
TheCodeYinChao <292306404@qq.com>
2020-11-27 15:08:00 +0800

    hashmap

---
+++ basic-thread/src/main/java/com/zyc/juccollcetion/ExampleHashMap.java
@@ -0,0 +1,17 @@
+package com.zyc.juccollcetion;
+
+import java.util.HashMap;
+
+/**
+ * dsc: ExampleHashMap
+ * date: 2020/11/26 18:56
+ * author: zyc
+ */
+public class ExampleHashMap {
+    public static void main(String[] args) {
+
+        HashMap hashMap = new HashMap();
+
+        hashMap.put("a","A");
+    }
+}
