69ea051ea1366a99d49b5161819987b1e0edbca0
zhangyinchao <292306404@qq.com>
2020-12-06 20:58:38 +0800

    classloader

--- basic-soundcode/src/main/java/com/zyc/spring/factorybean/DemoFacctoryBean.java
+++ basic-soundcode/src/main/java/com/zyc/spring/factorybean/DemoFacctoryBean.java
@@ -4,7 +4,15 @@ import org.springframework.beans.factory.FactoryBean;
 import org.springframework.stereotype.Component;
 
 /**
- * Created by Admin on 2020/1/5.
+ *  FactoryBean 获取的对象是 注入的包含两个对象
+ *   1 factorybean 本身
+ *   2 getObject 所提供方法的返回实例
+ *
+ *   如何获取？
+ *    1 & + ClassName  FactoryBean本身
+ *    2 getobject 在本类示例中 就是纳入 spring容器类的DaoDemo
+ *
+ *   有什么意义？
  */
 @Component("demoBean")
 public class DemoFacctoryBean implements FactoryBean {
--- basic-soundcode/src/main/java/com/zyc/spring/factorybean/TestDemo.java
+++ basic-soundcode/src/main/java/com/zyc/spring/factorybean/TestDemo.java
@@ -3,6 +3,7 @@ package com.zyc.spring.factorybean;
 import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by Admin on 2020/1/5.
@@ -11,16 +12,25 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class TestDemo {
 
     @Test
+    public void beanFactory() {
+
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("");
+
+        AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext("com.zyc.spring.factorybean");
+
+
+    }
+
+    @Test
     public void testFactoryBean() {
-        AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext("com.zyc.*.**");
+        AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext("com.zyc.spring.factorybean");
         //DemoFacctoryBean facctoryBean = (DemoFacctoryBean) configApplicationContext.getBean("demoBean");
         //java.lang.ClassCastException
-
         DaoDemo facctoryBean = (DaoDemo) configApplicationContext.getBean("demoBean");
         facctoryBean.test();
 
-
-        BeanFactory beanFactory;
+        DemoFacctoryBean facctoryBean1 = (DemoFacctoryBean) configApplicationContext.getBean("&demoBean");//& + 名称
+        System.out.println(facctoryBean1);
     }
 
 }
