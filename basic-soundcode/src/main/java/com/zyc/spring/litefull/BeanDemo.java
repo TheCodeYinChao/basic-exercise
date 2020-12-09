06343c1ef31ea9780c9be8a21eead56eee5c3579
TheCodeYinChao <292306404@qq.com>
2020-12-09 19:24:06 +0800

    spring执行流程full lite

---
+++ basic-soundcode/src/main/java/com/zyc/spring/litefull/BeanDemo.java
@@ -0,0 +1,11 @@
+package com.zyc.spring.litefull;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * dsc: BeanDemo
+ * date: 2020/12/7 11:19
+ * author: zyc
+ */
+public class BeanDemo {
+}
---
+++ basic-soundcode/src/main/java/com/zyc/spring/litefull/ImportResourceDemo.java
@@ -0,0 +1,14 @@
+package com.zyc.spring.litefull;
+
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Component;
+
+/**
+ * dsc: ImportResourceDemo
+ * date: 2020/12/9 16:36
+ * author: zyc
+ */
+@ImportResource("")//这个注解需要配合其他纳入容器的注解来使用
+@Component
+public class ImportResourceDemo {
+}
