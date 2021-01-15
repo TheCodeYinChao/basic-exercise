1e71fa7533f24a357237b919cd263ffcd2930fad
TheCodeYinChao <292306404@qq.com>
2021-01-15 14:44:07 +0800

    关于继承

---
+++ jvm/src/main/java/cn/zyc/关于继承/Base.java
@@ -0,0 +1,16 @@
+package cn.zyc.关于继承;
+
+/**
+ * dsc: Base
+ * date: 2021/1/15 14:15
+ * author: zyc
+ */
+public class Base {
+    private String msg;
+    public Base() {
+    }
+
+    public Base(String msg) {
+        this.msg = msg;
+    }
+}
---
+++ jvm/src/main/java/cn/zyc/关于继承/Demo.java
@@ -0,0 +1,12 @@
+package cn.zyc.关于继承;
+
+/**
+ * dsc: Demo 对于继承而言，子类可以扩展原有功能我们通过super来填充父类等的关键信息，
+ * date: 2021/1/15 14:24
+ * author: zyc
+ */
+public class Demo {
+    public static void main(String[] args) {
+         Base base = new BaseExt(1);
+    }
+}
