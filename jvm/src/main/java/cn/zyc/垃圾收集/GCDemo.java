6e74265f908c71252c1f383a2a170a28f60a2e61
TheCodeYinChao <292306404@qq.com>
2021-01-21 16:31:02 +0800

    gc局部代码块执行完成之后会回收

---
+++ jvm/src/main/java/cn/zyc/垃圾收集/GCDemo.java
@@ -0,0 +1,34 @@
+package cn.zyc.垃圾收集;
+
+import org.junit.Test;
+
+/**
+ * dsc: GCDemo
+ * date: 2021/1/21 16:12
+ * author: zyc 探究对象的回收
+ */
+public class GCDemo {
+    @Test
+    public void  test(){
+        Object p = new Object();
+
+        int i  =  1;
+    }
+    @Test
+    public void  test1(){
+       t();
+
+        int i  =  1;
+        while (true){
+
+        }
+    }
+
+    public void t(){
+        {
+            Object p = new Object();//这个对象在这个代码块执行完成之后
+            //如果触发gc的话 会回收
+        }
+        Object p = new Object();
+    }
+}
