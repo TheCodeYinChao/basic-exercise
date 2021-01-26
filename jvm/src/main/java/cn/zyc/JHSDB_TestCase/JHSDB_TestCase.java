4bb4300ea121b0513634fd36233fefc438751c8c
TheCodeYinChao <292306404@qq.com>
2021-01-26 17:28:28 +0800

    关于变量存储的位置

--- jvm/src/main/java/cn/zyc/JHSDB_TestCase/JHSDB_TestCase.java
+++ jvm/src/main/java/cn/zyc/JHSDB_TestCase/JHSDB_TestCase.java
@@ -15,9 +15,13 @@ public class JHSDB_TestCase {
     static class Test {
         //staticObj随着Test的类型信息存放在方法区
         static ObjectHolder staticObj = new ObjectHolder();
+        //finalstaticObj随着Test的类型信息存放在方法区
+        static final  ObjectHolder finalstaticObj = new ObjectHolder();
 
         //instanceObj随着Test的对象实例存放在Java堆
         ObjectHolder instanceObj = new ObjectHolder();
+        //finalinstanceObj随着Test的对象实例存放在Java堆
+        final ObjectHolder finalinstanceObj = new ObjectHolder();
         void foo() throws IOException {
             //localObject则是存放在foo()方法栈帧的局部变量表中
             ObjectHolder localObj = new ObjectHolder();
