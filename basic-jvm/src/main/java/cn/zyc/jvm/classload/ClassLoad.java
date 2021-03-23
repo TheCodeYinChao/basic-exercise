a50c25c390d124f6ba883e35638d128d24dc33ff
TheCodeYinChao <292306404@qq.com>
2021-03-23 17:12:34 +0800

    提交classloader研究

--- basic-jvm/src/main/java/cn/zyc/jvm/classload/ClassLoad.java
+++ basic-jvm/src/main/java/cn/zyc/jvm/classload/ClassLoad.java
@@ -5,8 +5,8 @@ package cn.zyc.jvm.classload;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.URLClassLoader;
 
 /**
  * ！！！ 不得精髓  要学会必须要先学会应用场景 不然学的快忘记的更快
@@ -17,9 +17,17 @@ import java.io.InputStream;
 public class ClassLoad extends ClassLoader {
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
-        String fileName = name.substring(name.lastIndexOf(".") + 1) + ".class";
+//        String fileName = name.substring(name.lastIndexOf(".") + 1) + ".class";
+        File file = new File("D:\\project\\basic-exercise\\basic-jvm\\JAVALoader\\test\\JavaB.class");
 
-        InputStream is = getClass().getResourceAsStream(fileName);
+        InputStream is = null;
+        try {
+            is = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+//        InputStream is = getClass().getResourceAsStream(fileName);
         if (is == null) {
             return super.loadClass(name);
         }
@@ -30,7 +38,7 @@ public class ClassLoad extends ClassLoader {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return defineClass(name, bytes, 0, bytes.length);
+        return defineClass("test.JavaB", bytes, 0, bytes.length);
     }
 
     @Override
@@ -45,8 +53,6 @@ public class ClassLoad extends ClassLoader {
 
         ClassLoad classLoad = new ClassLoad();
         Class<?> aClass = classLoad.loadClass("cn.zyc.jvm.classload.DemoClinet");
-
-
 
         Object o = aClass.newInstance();
         System.out.println(o);
@@ -68,22 +74,55 @@ public class ClassLoad extends ClassLoader {
 
     }
 
+    /**
+     * 子加载器可以使用父类加载器的东西（类） 父加载器不可以使用子类加载的东西（类） 继承
+     * @throws Exception
+     */
     @Test
     public  void test()throws Exception {
 
+
+        ClassLoader app = ClassLoader.getSystemClassLoader();
+        Class<B> aClass = (Class<B>) app.loadClass("cn.zyc.jvm.classload.B");
+        B b2 = aClass.newInstance();//当使用的类是一个classloader时 是可以转换的
+        b2.test();
+
+
+
         ClassLoad cl = new ClassLoad();
-
-        Class<B> bClass = B.class;
-
-        Class<B> b = (Class<B>) cl.loadClass("cn.zyc.jvm.classload.B");
-        B b1 = b.newInstance();
+        Class b =  cl.loadClass("cn.zyc.jvm.classload.B");
+        Object b1 = b.newInstance();
 
 
         Class<?> a = cl.loadClass("cn.zyc.jvm.classload.A");
 
+
         A a1 = (A) a.newInstance();
 
-        a1.b(b1);
+//        a1.b(b1);
 
     }
+
+
+    /**
+     * 1 什么样的类可以被加载？
+     *   通过javac 编译成标准的.class文件 ，不然会报魔术错误（校验时期）
+     * 2 如何加载一个非classpath的类？
+     *   定义好文件根路径 然后以包路径为class的类加载的名称
+     * 3 不同类加载器加载的类之间能使用吗？（不能） 命名空间不一致
+     *
+     *  两个类相互能用的前提是在一个classpath下
+     * 4 全盘委托会因为一个类引用了另一个类 类可以使用哪个类，用的同一个类加载器
+     *
+     *
+     * 5 当你决定创建你自己的ClassLoader时，需要继承java.lang.ClassLoader或者它的子类。
+     * 在实例化每个ClassLoader对象时，需要指定一个父对象；如果没有指定的话，
+     * 系统自动指定ClassLoader.getSystemClassLoader()为父对象。
+     *
+     *
+     *
+     *
+     *
+     */
+
 }
