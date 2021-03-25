61fb827eba67f8ac2724c361b6f43600165e17fd
TheCodeYinChao <292306404@qq.com>
2021-03-25 13:46:16 +0800

    类加载如何执行

--- basic-jvm/src/main/java/cn/zyc/jvm/classload/sort/Sub.java
+++ basic-jvm/src/main/java/cn/zyc/jvm/classload/sort/Sub.java
@@ -1,5 +1,8 @@
 package cn.zyc.jvm.classload.sort;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * dsc: Sub
  * date: 2021/3/25 11:25
@@ -7,6 +10,8 @@ package cn.zyc.jvm.classload.sort;
  */
 public class Sub extends Parent {
     static int c =1;
+    static final Map bb =new HashMap();
+    static final int bbbb = 0;
     static {
         int d =2;
     }
